ac07e397912859d4a33ddc514805d960bbba065b
“Karen <karen.arrieta1991@gmail.com>
2023-05-26 16:08:52 -0500

    Agregando segunda solucion

--- js-2/max.js
+++ js-2/max.js
@@ -1,5 +1,12 @@
 // escribe la función max acá
+function max(numeros){
+
+    let total = 0;
+    for(let i = 0; i < numeros.length; i++) total+=numeros[i];
+    return total;
+
+}
 
 console.log(max([1, 3, 2])) // 3
 console.log(max([10, 9, 8, 7, 6, 5, 4])) // 10
-console.log(max([])) // undefined
+console.log(max([])) // undefin
