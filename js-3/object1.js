d504d65647f53a0257a4b44fb80981cca86154ba
“Karen <karen.arrieta1991@gmail.com>
2023-05-29 09:43:05 -0500

    Ejercicio javaScript 3

---
+++ js-3/object1.js
@@ -0,0 +1,17 @@
+const pedro = {
+    nombre: "Pedro Perez",
+    edad: 30,
+    activo: true,
+    hobbies: ["programar", "squash", "piano"],
+  }
+  
+  console.log(pedro.edad)
+  pedro.estatura = 1.8
+  delete pedro.activo
+  
+  const keys = Object.keys(pedro)
+  
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    console.log(keys[i] + ":" , pedro[key])
+  }
